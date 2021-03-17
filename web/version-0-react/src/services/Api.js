a1c4b271bff25f51262f9ff4507eec4a9d243d86
dayana romero <dayana.romero88@gmail.com>
2021-03-17 20:47:05 +0100

    repo

--- web/version-0-react/src/services/Api.js
+++ web/version-0-react/src/services/Api.js
@@ -5,7 +5,7 @@ const apiUrl = isDevEnviroment
 
 function api(data) {
   console.log(apiUrl);
-  return fetch("http://localhost:3000/card", {
+  return fetch(apiUrl, {
     method: "POST",
     body: JSON.stringify(data),
     headers: {
